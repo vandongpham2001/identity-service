a81b2e7814e132975632d39ad9e9caf6a0274610
DongPV <vandongpham2001@gmail.com>
2025-04-24 23:56:11 +0700

    feat: update project configuration and dependencies, improve code formatting

--- src/main/java/com/dongpv/sns/identity/code/DisplayCodesStr.java
+++ src/main/java/com/dongpv/sns/identity/code/DisplayCodesStr.java
@@ -2,5 +2,6 @@ package com.dongpv.sns.identity.code;
 
 public interface DisplayCodesStr {
     String getCode();
+
     String getDisplay();
 }
--- src/main/java/com/dongpv/sns/identity/entity/BaseEntity.java
+++ src/main/java/com/dongpv/sns/identity/entity/BaseEntity.java
@@ -1,16 +1,17 @@
 package com.dongpv.sns.identity.entity;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-
-import java.io.Serializable;
-import java.util.Objects;
 
 @Getter
 @Setter

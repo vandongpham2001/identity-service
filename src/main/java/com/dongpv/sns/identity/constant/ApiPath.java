c4f7d4d9bf1b3e42b8614cf1328ed838136233ea
DongPV <vandongpham2001@gmail.com>
2025-05-23 09:09:31 +0700

    feat: add constant classes for API paths and predefined roles

---
+++ src/main/java/com/dongpv/sns/identity/constant/ApiPath.java
@@ -0,0 +1,8 @@
+package com.dongpv.sns.identity.constant;
+
+public class ApiPath {
+    public static final String BASE_API = "/api";
+    public static final String V1 = BASE_API + "/v1";
+
+    private ApiPath() {}
+}
---
+++ src/main/java/com/dongpv/sns/identity/constant/CommonConstant.java
@@ -0,0 +1,7 @@
+package com.dongpv.sns.identity.constant;
+
+public class CommonConstant {
+    public static final String ROLE_PREFIX = "ROLE_";
+
+    private CommonConstant() {}
+}

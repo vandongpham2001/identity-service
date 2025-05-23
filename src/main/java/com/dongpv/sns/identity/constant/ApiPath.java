a2ebc81bce50644493b807dab275b2e5ce9aa745
DongPV <vandongpham2001@gmail.com>
2025-05-23 09:19:28 +0700

    feat: add V2 API path constant to ApiPath class

--- src/main/java/com/dongpv/sns/identity/constant/ApiPath.java
+++ src/main/java/com/dongpv/sns/identity/constant/ApiPath.java
@@ -3,6 +3,7 @@ package com.dongpv.sns.identity.constant;
 public class ApiPath {
     public static final String BASE_API = "/api";
     public static final String V1 = BASE_API + "/v1";
+    public static final String V2 = BASE_API + "/v2";
 
     private ApiPath() {}
 }

63f7bafe1b43da4b48a60625ecc089630623e76f
DongPV <vandongpham2001@gmail.com>
2025-10-20 23:52:26 +0700

    chore: test pre commit

---
+++ src/main/java/com/dongpv/sns/identity/constant/ApiVersionConstant.java
@@ -0,0 +1,10 @@
+package com.dongpv.sns.identity.constant;
+
+public class ApiVersionConstant {
+
+    public static final String V1 = "v1";
+
+    public static final String V2 = "v2";
+
+    private ApiVersionConstant() {}
+}
